4408addcdf38b5a3214bad41ae8175acf6d0b152
kiescha <90151742+kiescha@users.noreply.github.com>
2022-01-25 08:43:10 +0100

    task 3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,13 +7,17 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-            ],
+            movies: [],
             selectedMovie: null
     }
+  }
+  componentDidMount() {
+    axios.get('https://manoflixdb.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -26,7 +31,7 @@ export class MainView extends React.Component {
     const { movies, selectedMovie } = this.state;
 
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view"/>
 
     return (
       <div className="main-view">
