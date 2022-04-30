4abdd93d666fcdf56eb281c54199522b54dba132
kiescha <90151742+kiescha@users.noreply.github.com>
2022-04-30 20:26:26 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -19,7 +19,7 @@ export class MainView extends React.Component {
   
   getMovies(token){
     axios.get("https://manoflixdb.herokuapp.com/movies", {
-      headers: {Authorization: `Bearer ${token}`}
+      headers: {Authorization: `Bearer${token}`}
     })
     .then(response => {
       this.setState({
